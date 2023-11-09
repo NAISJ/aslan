d6cea696674865f52c3ba237cdeac8d39b9b6067
Stem Stem <stem@astanait.edu.kz>
2023-11-09 13:11:58 +0600

    v1.3 psycopg2 was added

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-num = 334
-for i in range(11):
-   print(num, 'x', i, '=', num*i)
+#THIS IS NEW VERSION TO CONNECT TO POSTGRESQL
+import psycopg2
+
+conn= psycopg2.connect(host = 'localhost'
+                       database = 'postgress'
+                       username = 'postgress'
+                       password = '1')
+cur = conn.cursor()
+cur.execute('SELECT superhero_name FROM superhero.superhero LIMIT 5;')
+usernames = [r[0] for i in cur.fetchall()]
+Found= False
+while not Found:
+   username = input('Введите ваш логин:')
+   if username in usernames:
+      print('Вы есть в списке')
+      Found=True
+      else:
+         print('Вас нет в списке')
+
+conn.commit()
+cur.close()
+conn.close()
+                  
